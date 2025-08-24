fe5ca3cc577aa337ee045985cf37cf2f48f94059
nanaios <game.ryd.131@gmail.com>
2025-08-24 18:44:59 +0900

    mixinを使えば何とかなる？

---
+++ src/main/java/com/nanaios/AppliedAmmoBox/mixin/MixinGridLinkables.java
@@ -0,0 +1,24 @@
+package com.nanaios.AppliedAmmoBox.mixin;
+
+import appeng.api.features.GridLinkables;
+import appeng.api.features.IGridLinkableHandler;
+import com.nanaios.AppliedAmmoBox.item.WirelessAmmoBoxItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = GridLinkables.class,remap = false)
+public class MixinGridLinkables {
+    @Inject(method = "get",at=@At("HEAD"),cancellable = true)
+    private static void mixinGridLinkables$get(ItemLike itemLike, CallbackInfoReturnable<IGridLinkableHandler> cir) {
+
+        Item itemInMixin = itemLike.asItem();
+        if(itemInMixin != null && itemInMixin instanceof WirelessAmmoBoxItem) {
+            System.out.println("item instanceof ammobox!");
+            cir.setReturnValue(WirelessAmmoBoxItem.LINKABLE_HANDLER);
+        }
+    }
+}
