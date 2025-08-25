ede80fd61f17d6506090295c4edc0e66cfb5be41
nanaios <game.ryd.131@gmail.com>
2025-08-25 15:18:00 +0900

    Redirectを増やした

--- src/main/java/com/nanaios/AppliedAmmoBox/item/WirelessAmmoBoxItem.java
+++ src/main/java/com/nanaios/AppliedAmmoBox/item/WirelessAmmoBoxItem.java
@@ -3,7 +3,6 @@ package com.nanaios.AppliedAmmoBox.item;
 import appeng.api.features.IGridLinkableHandler;
 import appeng.core.localization.GuiText;
 import appeng.core.localization.Tooltips;
-import com.mojang.datafixers.util.Pair;
 import com.nanaios.AppliedAmmoBox.AppliedAmmoBox;
 import com.tacz.guns.api.DefaultAssets;
 import com.tacz.guns.api.TimelessAPI;
@@ -15,18 +14,11 @@ import com.tacz.guns.api.item.nbt.AmmoBoxItemDataAccessor;
 import com.tacz.guns.config.sync.SyncConfig;
 import com.tacz.guns.inventory.tooltip.AmmoBoxTooltip;
 import net.minecraft.ChatFormatting;
-import net.minecraft.Util;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.core.GlobalPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.SlotAccess;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
@@ -34,11 +26,7 @@ import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 import javax.annotation.Nullable;
@@ -47,19 +35,7 @@ import java.util.Optional;
 
 public class WirelessAmmoBoxItem extends Item implements DyeableLeatherItem, AmmoBoxItemDataAccessor,ILinkableItem,IExtraAmmoBox {
 
-    //private static final Logger LOG = LoggerFactory.getLogger(WirelessAmmoBoxItem.class);
-
     public static final IGridLinkableHandler LINKABLE_HANDLER = new LinkableHandler();
-
-    //private static final String TAG_ACCESS_POINT_POS = "accessPoint";
-
-    //public static final int IRON_LEVEL = 0;
-
-    //private static final String DISPLAY_TAG = "display";
-    //private static final String COLOR_TAG = "color";
-
-    //private static final int OPEN = 0;
-    //private static final int CLOSE = 1;
 
     public WirelessAmmoBoxItem() {
         super(new Properties().stacksTo(1));
@@ -75,16 +51,6 @@ public class WirelessAmmoBoxItem extends Item implements DyeableLeatherItem, Amm
     public void setAmmoCount(ItemStack ammoBox, int count) {
         AmmoBoxItemDataAccessor.super.setAmmoCount(ammoBox, count);
     }
-
-    /* @OnlyIn(Dist.CLIENT)
-    public static int getColor(ItemStack stack, int tintIndex) {
-        return tintIndex > 0 ? -1 : getTagColor(stack);
-    } */
-
-    /* @OnlyIn(Dist.CLIENT)
-    public static float getStatue(ItemStack stack, @Nullable ClientLevel level, @Nullable LivingEntity entity, int seed) {
-        return 8;
-    } */
 
     @Override
     public boolean isAmmoBoxOfGun(ItemStack gun, ItemStack ammoBox) {
@@ -102,25 +68,6 @@ public class WirelessAmmoBoxItem extends Item implements DyeableLeatherItem, Amm
         //return AmmoBoxItemDataAccessor.super.isAmmoBoxOfGun(gun, ammoBox);
     }
 
-    /* private static int getOpenStatue(ItemStack stack, IAmmoBox iAmmoBox) {
-        boolean idIsEmpty = iAmmoBox.getAmmoId(stack).equals(DefaultAssets.EMPTY_AMMO_ID);
-        boolean countIsZero = iAmmoBox.getAmmoCount(stack) <= 0;
-        if (idIsEmpty || countIsZero) {
-            return OPEN;
-        }
-        return CLOSE;
-    } */
-
-    /* private static int getLevelStatue(ItemStack stack, IAmmoBox iAmmoBox) {
-        return iAmmoBox.getAmmoLevel(stack);
-    } */
-
-    /* private static int getTagColor(ItemStack stack) {
-
-        CompoundTag compoundtag = stack.getTagElement(DISPLAY_TAG);
-        return compoundtag != null && compoundtag.contains(COLOR_TAG, Tag.TAG_ANY_NUMERIC) ? compoundtag.getInt(COLOR_TAG) : 0x727d6b;
-    } */
-
     @Override
     public boolean overrideOtherStackedOnMe(@NotNull ItemStack stack, @NotNull ItemStack pOther, @NotNull Slot slot, @NotNull ClickAction action, @NotNull Player player, @NotNull SlotAccess access) {
         return super.overrideOtherStackedOnMe(stack, pOther, slot, action, player, access);
@@ -133,32 +80,6 @@ public class WirelessAmmoBoxItem extends Item implements DyeableLeatherItem, Amm
             // 点击的格子
             ItemStack slotItem = slot.getItem();
             ResourceLocation boxAmmoId = this.getAmmoId(ammoBox);
-
-            // 格子为空，那就是取出物品
-            /* if (slotItem.isEmpty()) {
-                // 啥也没有，不能取出
-                if (boxAmmoId.equals(DefaultAssets.EMPTY_AMMO_ID)) {
-                    return false;
-                }
-                // 数量不对，不能取出
-                int boxAmmoCount = this.getAmmoCount(ammoBox);
-                if (boxAmmoCount <= 0) {
-                    return false;
-                }
-                TimelessAPI.getCommonAmmoIndex(boxAmmoId).ifPresent(index -> {
-                    int takeCount = Math.min(index.getStackSize(), boxAmmoCount);
-                    ItemStack takeAmmo = AmmoItemBuilder.create().setId(boxAmmoId).setCount(takeCount).build();
-                    slot.safeInsert(takeAmmo);
-
-                    int remainCount = boxAmmoCount - takeCount;
-                    this.setAmmoCount(ammoBox, remainCount);
-                    if (remainCount <= 0) {
-                        this.setAmmoId(ammoBox, DefaultAssets.EMPTY_AMMO_ID);
-                    }
-                    this.playRemoveOneSound(player);
-                });
-                return true;
-            } */
 
             // 如果是子弹
             if (slotItem.getItem() instanceof IAmmo iAmmo) {
@@ -188,10 +109,6 @@ public class WirelessAmmoBoxItem extends Item implements DyeableLeatherItem, Amm
         }
         return false;
     }
-
-    /* private void playRemoveOneSound(Entity entity) {
-        entity.playSound(SoundEvents.BUNDLE_REMOVE_ONE, 0.8F, 0.8F + entity.level().getRandom().nextFloat() * 0.4F);
-    } */
 
     private void playInsertSound(Entity entity) {
         entity.playSound(SoundEvents.BUNDLE_INSERT, 0.8F, 0.8F + entity.level().getRandom().nextFloat() * 0.4F);
--- src/main/java/com/nanaios/AppliedAmmoBox/mixin/MixinAbstractGunItem.java
+++ src/main/java/com/nanaios/AppliedAmmoBox/mixin/MixinAbstractGunItem.java
@@ -15,8 +15,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = AbstractGunItem.class,remap = false)
 public class MixinAbstractGunItem {
-    @Redirect(method = "lambda$canReload$1(Lnet/minecraft/world/item/ItemStack;Lnet/minecraftforge/items/IItemHandler;)Ljava/lang/Boolean;",at= @At(value = "INVOKE", target = "Lcom/tacz/guns/api/item/IAmmoBox;isAmmoBoxOfGun(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
-    private static boolean redirectIsAmmoBoxOfGun(IAmmoBox ammoBox, ItemStack gunItem, ItemStack checkAmmoStack) {
+    @Redirect(method = "lambda$canReload$1",at= @At(value = "INVOKE", target = "Lcom/tacz/guns/api/item/IAmmoBox;isAmmoBoxOfGun(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private static boolean redirectIsAmmoBoxOfGun1(IAmmoBox ammoBox, ItemStack gunItem, ItemStack checkAmmoStack) {
+        AppliedAmmoBox.LOGGER.info("redirectIsAmmoBoxOfGun1 calling!");
+        return ((IExtraAmmoBox)ammoBox).isAmmoBoxOfGunWithExtra(gunItem,checkAmmoStack,1);
+    }
+
+    @Redirect(method = "findAndExtractInventoryAmmo",at= @At(value = "INVOKE", target = "Lcom/tacz/guns/api/item/IAmmoBox;isAmmoBoxOfGun(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private boolean redirectIsAmmoBoxOfGun2(IAmmoBox ammoBox, ItemStack gunItem, ItemStack checkAmmoStack) {
+        AppliedAmmoBox.LOGGER.info("redirectIsAmmoBoxOfGun2 calling!");
+        return ((IExtraAmmoBox)ammoBox).isAmmoBoxOfGunWithExtra(gunItem,checkAmmoStack,1);
+    }
+
+    @Redirect(method = "lambda$hasInventoryAmmo$6",at= @At(value = "INVOKE", target = "Lcom/tacz/guns/api/item/IAmmoBox;isAmmoBoxOfGun(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
+    private static boolean redirectIsAmmoBoxOfGun3(IAmmoBox ammoBox, ItemStack gunItem, ItemStack checkAmmoStack) {
+        AppliedAmmoBox.LOGGER.info("redirectIsAmmoBoxOfGun3 calling!");
         return ((IExtraAmmoBox)ammoBox).isAmmoBoxOfGunWithExtra(gunItem,checkAmmoStack,1);
     }
 }
