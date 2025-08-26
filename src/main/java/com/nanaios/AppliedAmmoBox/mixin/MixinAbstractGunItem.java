aae11964b09b7aaf21782f8d9b89a108de402a24
nanaios <game.ryd.131@gmail.com>
2025-08-27 00:21:20 +0900

    Mixin改変成功1

--- src/main/java/com/nanaios/AppliedAmmoBox/mixin/MixinAbstractGunItem.java
+++ src/main/java/com/nanaios/AppliedAmmoBox/mixin/MixinAbstractGunItem.java
@@ -2,6 +2,7 @@ package com.nanaios.AppliedAmmoBox.mixin;
 
 import com.nanaios.AppliedAmmoBox.AppliedAmmoBox;
 import com.nanaios.AppliedAmmoBox.item.IExtraAmmoBox;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.tacz.guns.api.item.IAmmoBox;
 import com.tacz.guns.api.item.gun.AbstractGunItem;
 import net.minecraft.world.item.ItemStack;
@@ -14,12 +15,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = AbstractGunItem.class,remap = false)
 public class MixinAbstractGunItem {
-    @Inject(method = "lambda$canReload$1",at= @At(value = "INVOKE", target = "Lnet/minecraftforge/items/IItemHandler;getStackInSlot(I)Lnet/minecraft/world/item/ItemStack;",shift = At.Shift.AFTER),cancellable = true)
-    private static void mixinAbstractGunItem$lambda$canReload$1(ItemStack gunItem, IItemHandler cap ,CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "lambda$canReload$1", at= @At(value = "INVOKE", target = "Lnet/minecraftforge/items/IItemHandler;getStackInSlot(I)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER ),cancellable = true)
+    private static void mixinAbstractGunItem$lambda$canReload$1(ItemStack gunItem, IItemHandler cap, CallbackInfoReturnable<Boolean> cir,@Local int i) {
         //AppliedAmmoBox.LOGGER.info("redirectIsAmmoBoxOfGun1 calling!");
-        /* if (checkAmmoStack.getItem() instanceof IExtraAmmoBox iAmmoBox && iAmmoBox.isAmmoBoxOfGunWithExtra(gunItem, checkAmmoStack,0)) {
+        ItemStack checkAmmoStackInMinx = cap.getStackInSlot(i);
+        if (checkAmmoStackInMinx.getItem() instanceof IExtraAmmoBox iAmmoBox && iAmmoBox.isAmmoBoxOfGunWithExtra(gunItem, checkAmmoStackInMinx,1)) {
             cir.setReturnValue(true);
-        } */
+        }
     }
 
     @Redirect(method = "findAndExtractInventoryAmmo",at= @At(value = "INVOKE", target = "Lcom/tacz/guns/api/item/IAmmoBox;isAmmoBoxOfGun(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemStack;)Z"))
